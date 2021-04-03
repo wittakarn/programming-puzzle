1cfca2814dc4a99e695beefcd62b816b2cd6a027
wittakarn <wittakarn.expert@gmail.com>
2021-04-03 22:59:22 +0700

    * Finish to-do-list with python
    * Finish to-do-list with typescript

--- to-do-list/example_redis.py
+++
@@ -1,9 +0,0 @@
-#import the library
-import redis
-# Create connection object
-r = redis.Redis(host='us1-vast-phoenix-32654.upstash.io', port=32654, password='63e965ebc06a46509d28990f0536fc48')
-# set a value for the foo object
-r.set('foo', 'bar')
-# retrieve and print the value for the foo object
-print(r.get('foo'))
-print(r.get('key'))
